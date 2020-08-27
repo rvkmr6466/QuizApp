309d2e7a4cc6e2ad932cb50f77ebb9b66a2c0b40
Ravi Kumar <Ravi Kumar>
2020-08-27 23:29:18 +0530

    created navbar, quiz, register, and result component also quiz service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { ResultComponent } from './result/result.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    ResultComponent,
+    QuizComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
