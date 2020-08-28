0e22a67d5c93120804732d3a71406a150abea78a
Ravi Kumar <Ravi Kumar>
2020-08-28 13:05:59 +0530

    form changes implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +20,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
-		FormsModule
+		ReactiveFormsModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
 	selector: 'app-register',
@@ -7,11 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-	emailPattern = '^[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$';
+	// emailPattern = '^[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$';
+	genders = ['male', 'female'];
+	signupForm: FormGroup;
 
 	constructor() { }
 
 	ngOnInit(): void {
+		this.signupForm = new FormGroup({
+			//dont call required function, its just want reference and will call the function when it will detect changes
+			'userData': new FormGroup({
+				'username': new FormControl(null, Validators.required), 
+				'email': new FormControl(null, [Validators.required, Validators.email])
+			}),
+			'gender': new FormControl('male'),
+			'hobbies': new FormArray([])
+		});
 	}
 
+	onSubmit() {
+		console.log(this.signupForm);	
+	}
+
+	onAddHobby() {
+		const hobbiesControl = new FormControl(null, Validators.required);
+		(<FormArray>this.signupForm.get('hobbies')).push(hobbiesControl); 
+
+	}
 }
